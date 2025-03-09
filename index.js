669caa2c053170283b56b4b370afcf96859f770c
Kazi Arman <153831689+kaziarman23@users.noreply.github.com>
2025-03-09 11:10:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+const path = require("path");
+const HomeRoute = require("./Routers/HomeRoute.js");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", HomeRoute);
+
+app.listen(port, () => {
+  console.log(`backend server is runing on : ${port}`);
+});
+
+
